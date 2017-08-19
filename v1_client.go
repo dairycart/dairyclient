36ceedc9858a6b72f687a40f6a43e38e7bbba5dd
Jeffrey D <verygoodsoftwarenotvirus@users.noreply.github.com>
2017-08-19 04:21:09 -0500

    Revise how we build URLs

--- v1_client.go
+++ v1_client.go
@@ -62,6 +62,18 @@ func (dc *V1Client) executeRequest(req *http.Request) (*http.Response, error) {
 	return dc.Do(req)
 }
 
+func (dc *V1Client) buildURL(queryParams map[string]string, parts ...string) string {
+	parts = append([]string{currentAPIVersion}, parts...)
+	u, _ := url.Parse(strings.Join(parts, "/"))
+	queryString := mapToQueryValues(queryParams)
+	u.RawQuery = queryString.Encode()
+	return dc.URL.ResolveReference(u).String()
+}
+
+// BuildURL is the same as the unexported build URL, except I trust myself to never call the
+// unexported function with variables that could lead to an error being returned. This function
+// returns the error in the event a user needs to build an API url, but tries to do so with an
+// invalid value.
 func (dc *V1Client) BuildURL(queryParams map[string]string, parts ...string) (string, error) {
 	parts = append([]string{currentAPIVersion}, parts...)
 
@@ -130,11 +142,7 @@ func (dc *V1Client) DeleteUser(userID uint64) error {
 ////////////////////////////////////////////////////////
 
 func (dc *V1Client) ProductExists(sku string) (bool, error) {
-	u, err := dc.BuildURL(nil, "product", sku)
-	if err != nil {
-		return false, err
-	}
-
+	u := dc.buildURL(nil, "product", sku)
 	req, _ := http.NewRequest(http.MethodHead, u, nil)
 	res, err := dc.executeRequest(req)
 	if err != nil {
@@ -148,13 +156,8 @@ func (dc *V1Client) ProductExists(sku string) (bool, error) {
 }
 
 func (dc *V1Client) GetProduct(sku string) (*Product, error) {
-	u, err := dc.BuildURL(nil, "product", sku)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product", sku)
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
-
 	res, err := dc.executeRequest(req)
 	if err != nil {
 		return nil, err
@@ -169,11 +172,7 @@ func (dc *V1Client) GetProduct(sku string) (*Product, error) {
 }
 
 func (dc *V1Client) GetProducts(queryFilter map[string]string) (*http.Response, error) {
-	u, err := dc.BuildURL(queryFilter, "products")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(queryFilter, "products")
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
@@ -184,11 +183,7 @@ func (dc *V1Client) CreateProduct(np ProductCreationInput) (*http.Response, erro
 		return nil, err
 	}
 
-	u, err := dc.BuildURL(nil, "product")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product")
 	req, _ := http.NewRequest(http.MethodPost, u, body)
 	return dc.executeRequest(req)
 }
@@ -199,21 +194,13 @@ func (dc *V1Client) UpdateProduct(sku string, up ProductUpdateInput) (*http.Resp
 		return nil, err
 	}
 
-	u, err := dc.BuildURL(nil, "product", sku)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product", sku)
 	req, _ := http.NewRequest(http.MethodPatch, u, body)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) DeleteProduct(sku string) (*http.Response, error) {
-	u, err := dc.BuildURL(nil, "product", sku)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product", sku)
 	req, _ := http.NewRequest(http.MethodDelete, u, nil)
 	return dc.executeRequest(req)
 }
@@ -226,32 +213,20 @@ func (dc *V1Client) DeleteProduct(sku string) (*http.Response, error) {
 
 func (dc *V1Client) GetProductRoot(rootID uint64) (*http.Response, error) {
 	rootIDString := convertIDToString(rootID)
-	u, err := dc.BuildURL(nil, "product_root", rootIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_root", rootIDString)
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) GetProductRoots(queryFilter map[string]string) (*http.Response, error) {
-	u, err := dc.BuildURL(queryFilter, "product_roots")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(queryFilter, "product_roots")
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) DeleteProductRoot(rootID uint64) (*http.Response, error) {
 	rootIDString := convertIDToString(rootID)
-	u, err := dc.BuildURL(nil, "product_root", rootIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_root", rootIDString)
 	req, _ := http.NewRequest(http.MethodDelete, u, nil)
 	return dc.executeRequest(req)
 }
@@ -264,11 +239,7 @@ func (dc *V1Client) DeleteProductRoot(rootID uint64) (*http.Response, error) {
 
 func (dc *V1Client) GetProductOptions(productID uint64, queryFilter map[string]string) (*http.Response, error) {
 	productIDString := convertIDToString(productID)
-	u, err := dc.BuildURL(queryFilter, "product", productIDString, "options")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(queryFilter, "product", productIDString, "options")
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
@@ -276,11 +247,7 @@ func (dc *V1Client) GetProductOptions(productID uint64, queryFilter map[string]s
 func (dc *V1Client) CreateProductOptionForProduct(productID uint64, JSONBody string) (*http.Response, error) {
 	productIDString := convertIDToString(productID)
 	body := strings.NewReader(JSONBody)
-	u, err := dc.BuildURL(nil, "product", productIDString, "options")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product", productIDString, "options")
 	req, _ := http.NewRequest(http.MethodPost, u, body)
 	return dc.executeRequest(req)
 }
@@ -288,22 +255,14 @@ func (dc *V1Client) CreateProductOptionForProduct(productID uint64, JSONBody str
 func (dc *V1Client) UpdateProductOption(optionID uint64, JSONBody string) (*http.Response, error) {
 	optionIDString := convertIDToString(optionID)
 	body := strings.NewReader(JSONBody)
-	u, err := dc.BuildURL(nil, "product_options", optionIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_options", optionIDString)
 	req, _ := http.NewRequest(http.MethodPatch, u, body)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) DeleteProductOption(optionID uint64) (*http.Response, error) {
 	optionIDString := convertIDToString(optionID)
-	u, err := dc.BuildURL(nil, "product_options", optionIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_options", optionIDString)
 	req, _ := http.NewRequest(http.MethodDelete, u, nil)
 	return dc.executeRequest(req)
 }
@@ -317,11 +276,7 @@ func (dc *V1Client) DeleteProductOption(optionID uint64) (*http.Response, error)
 func (dc *V1Client) createProductOptionValueForOption(optionID uint64, JSONBody string) (*http.Response, error) {
 	optionIDString := convertIDToString(optionID)
 	body := strings.NewReader(JSONBody)
-	u, err := dc.BuildURL(nil, "product_options", optionIDString, "value")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_options", optionIDString, "value")
 	req, _ := http.NewRequest(http.MethodPost, u, body)
 	return dc.executeRequest(req)
 }
@@ -329,22 +284,14 @@ func (dc *V1Client) createProductOptionValueForOption(optionID uint64, JSONBody 
 func (dc *V1Client) updateProductOptionValueForOption(valueID uint64, JSONBody string) (*http.Response, error) {
 	valueIDString := convertIDToString(valueID)
 	body := strings.NewReader(JSONBody)
-	u, err := dc.BuildURL(nil, "product_option_values", valueIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_option_values", valueIDString)
 	req, _ := http.NewRequest(http.MethodPatch, u, body)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) deleteProductOptionValueForOption(optionID uint64) (*http.Response, error) {
 	optionIDString := convertIDToString(optionID)
-	u, err := dc.BuildURL(nil, "product_option_values", optionIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "product_option_values", optionIDString)
 	req, _ := http.NewRequest(http.MethodDelete, u, nil)
 	return dc.executeRequest(req)
 }
@@ -357,31 +304,19 @@ func (dc *V1Client) deleteProductOptionValueForOption(optionID uint64) (*http.Re
 
 func (dc *V1Client) getDiscountByID(discountID uint64) (*http.Response, error) {
 	discountIDString := convertIDToString(discountID)
-	u, err := dc.BuildURL(nil, "discount", discountIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "discount", discountIDString)
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) getListOfDiscounts(queryFilter map[string]string) (*http.Response, error) {
-	u, err := dc.BuildURL(queryFilter, "discounts")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(queryFilter, "discounts")
 	req, _ := http.NewRequest(http.MethodGet, u, nil)
 	return dc.executeRequest(req)
 }
 
 func (dc *V1Client) createDiscount(JSONBody string) (*http.Response, error) {
-	u, err := dc.BuildURL(nil, "discount")
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "discount")
 	body := strings.NewReader(JSONBody)
 	req, _ := http.NewRequest(http.MethodPost, u, body)
 	return dc.executeRequest(req)
@@ -389,11 +324,7 @@ func (dc *V1Client) createDiscount(JSONBody string) (*http.Response, error) {
 
 func (dc *V1Client) updateDiscount(discountID uint64, JSONBody string) (*http.Response, error) {
 	discountIDString := convertIDToString(discountID)
-	u, err := dc.BuildURL(nil, "discount", discountIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "discount", discountIDString)
 	body := strings.NewReader(JSONBody)
 	req, _ := http.NewRequest(http.MethodPatch, u, body)
 	return dc.executeRequest(req)
@@ -401,11 +332,7 @@ func (dc *V1Client) updateDiscount(discountID uint64, JSONBody string) (*http.Re
 
 func (dc *V1Client) deleteDiscount(discountID uint64) (*http.Response, error) {
 	discountIDString := convertIDToString(discountID)
-	u, err := dc.BuildURL(nil, "discount", discountIDString)
-	if err != nil {
-		return nil, err
-	}
-
+	u := dc.buildURL(nil, "discount", discountIDString)
 	req, _ := http.NewRequest(http.MethodDelete, u, nil)
 	return dc.executeRequest(req)
 }
